87637eb22fa2791a3e9e62dbeb6054359417424f
Amy Lim <amylim32897@gmail.com>
2022-02-11 23:10:49 +0900

    엔티티 매핑

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -4,7 +4,6 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
-import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -16,15 +15,13 @@ public class JpaMain {
         tx.begin();
 
         try {
-            // 영속
-            Member member = em.find(Member.class, 150L);
-            member.setName("AAAAA");
+            Member member = new Member();
+            member.setId(1L);
+            member.setUsername("A");
+            member.setRoleType(RoleType.USER);
 
-            em.clear();
+            em.persist(member);
 
-            Member member2 = em.find(Member.class, 150L);
-
-            System.out.println("=========================");
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
