e59db6781434e8d75f6f00d79d268bde33e980d0
Amy Lim <amylim32897@gmail.com>
2022-02-19 23:55:25 +0900

    main class

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -15,28 +15,6 @@ public class JpaMain {
         tx.begin();
 
         try {
-
-            Member member1 = new Member();
-            member1.setUsername("A");
-
-            Member member2 = new Member();
-            member2.setUsername("B");
-
-            Member member3 = new Member();
-            member3.setUsername("C");
-
-            System.out.println("=================");
-
-            em.persist(member1); // 1, 51
-            em.persist(member2); // memory
-            em.persist(member3); // memory
-
-            System.out.println("member1.getId() = " + member1.getId());
-            System.out.println("member2.getId() = " + member2.getId());
-            System.out.println("member3.getId() = " + member3.getId());
-
-            System.out.println("=================");
-
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
