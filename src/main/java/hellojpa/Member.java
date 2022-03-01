79acdc0ed2c0549489576f04c43a2f72b53fccf4
Amy Lim <amylim32897@gmail.com>
2022-03-01 13:45:03 +0900

    양방향 매핑

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -39,7 +39,8 @@ public class Member {
         return team;
     }
 
-    public void setTeam(Team team) {
+    public void changeTeam(Team team) { // 연관관계 메서드 // 단순한 getter setter가 아님을 나타내기 위해 이름 변경
         this.team = team;
+        team.getMembers().add(this);
     }
 }
