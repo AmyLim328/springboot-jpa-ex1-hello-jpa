956ce5a2183214a48ec45dde73ec72139c3650c4
Amy Lim <amylim32897@gmail.com>
2022-03-07 18:18:27 +0900

    main class

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -4,7 +4,6 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
-import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -16,7 +15,6 @@ public class JpaMain {
         tx.begin();
 
         try {
-
             Member member = new Member();
             member.setUsername("member1");
 
