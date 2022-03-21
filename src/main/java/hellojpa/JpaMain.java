a3e9e7d5eb6908ba7455a7963a2460af10bed4cb
Amy Lim <amylim32897@gmail.com>
2022-03-21 23:01:35 +0900

    JPA Criteria

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -4,6 +4,9 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 import java.util.List;
 
 public class JpaMain {
@@ -14,11 +17,25 @@ public class JpaMain {
         tx.begin();
 
         try {
-            List<Member> result = em.createQuery("select m from Member m where m.username like '%kim'", Member.class)
-                    .getResultList();
-            for (Member member : result) {
-                System.out.println("member = " + member);
+            // Criteria 사용 준비
+            CriteriaBuilder cb = em.getCriteriaBuilder();
+            CriteriaQuery<Member> query = cb.createQuery(Member.class);
+
+            // 루트 클래스 (조회를 시작할 클래스)
+            Root<Member> m = query.from(Member.class);
+
+            // 쿼리 생성
+            CriteriaQuery<Member> cq = query.select(m);
+
+            // 동적 쿼리
+            String username = "abc";
+            if (username != null) {
+                cq = cq.where(cb.equal(m.get("username"), "kim"));
             }
+
+            List<Member> resultList = em.createQuery(cq).getResultList();
+
+            // 실무에서 Criteria 사용하길 권장하지 않음 sql스럽지 않고 알아보기 어려워서 유지보수 힘듦
 
             tx.commit();
         } catch (Exception e) {
